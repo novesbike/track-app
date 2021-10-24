66d13de8e6f26b268040fa042716b61924487c63
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-24 19:07:10 -0300

    Vic/register screen (#21)
    * feat(navigator): reorder navigation options
    
    * feat(sign in): create register screen
    
    * feat(navigation screen): reorder screens & rename
    
    * feat(index): set register click navigate
    
    * feat(forgot password): set register click navigate

--- screens/signIn/register.js
+++ screens/signIn/register.js
@@ -8,42 +8,44 @@ import {TouchableOpacity} from "react-native-gesture-handler";
 import LOGO from "../../logos/noves-logo.png";
 import SocialButtons from "../../components/socialButtons";
 
-export default function SignIn({navigation}) {
-    const [input, setInput] = useState("");
-    const [hidePass, setHidePass] = useState(true);
+export default function Register({navigation}) {
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [hidePassword, setHidePassword] = useState(true);
 
     const Links = () => (
         <View style={styles.links}>
             <Text
                 style={{color: "#FF7300", fontSize: 12}}
-                onPress={() => navigation.navigate('ForgotPass')}
+                onPress={() => navigation.navigate('SignIn')}
             >
-                Esqueceu a senha?
-            </Text>
-            <Text
-                style={{color: "#FF7300", fontSize: 12}}
-                onPress={() => navigation.navigate("SignUp")}
-            >
-                Não tem conta? Cadastre-se
+                Já possui conta? Faça login
             </Text>
         </View>
     );
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{flex: 1, width: "100%", alignItems: "center"}}>
-                <Image source={LOGO} style={{width: 300, marginTop: 40}}/>
+            <View style={{flex: 1, width: "100%", alignItems: "center", justifyContent: "center"}}>
+                <Image source={LOGO} style={{width: 300}}/>
             </View>
 
             <View style={styles.loginbox}>
                 <View>
-                    <Text style={styles.title}>Login</Text>
+                    <Text style={styles.title}>Cadastre-se</Text>
                     <Text style={{color: "#616161", fontSize: 14}}>
-                        Faça login com o seu email e senha
+                        Faça o seu cadastro com o email e senha
                     </Text>
                 </View>
 
                 <View style={styles.subcontainer}>
+                    <View style={styles.containerInput}>
+                        <TextInput placeholder="Nome" style={styles.labelInput}/>
+                        <View style={{padding: 5}}>
+                            <Icon name="account" size={20} color="#757575"/>
+                        </View>
+                    </View>
+
                     <View style={styles.containerInput}>
                         <TextInput placeholder="E-mail" style={styles.labelInput}/>
                         <View style={{padding: 5}}>
@@ -51,23 +53,33 @@ export default function SignIn({navigation}) {
                         </View>
                     </View>
 
-                    <View style={[styles.containerInput, {marginBottom: 15}]}>
+                    <View style={[styles.containerInput]}>
                         <TextInput
                             style={styles.labelInput}
                             placeholder="Senha"
-                            value={input}
-                            onChangeText={(value) => setInput(value)}
-                            secureTextEntry={hidePass}
+                            value={password}
+                            onChangeText={(value) => setPassword(value)}
+                            secureTextEntry={hidePassword}
                         />
-                        <TouchableOpacity onPress={() => setHidePass(!hidePass)}>
+                        <TouchableOpacity onPress={() => setHidePassword(!hidePassword)}>
                             <View style={{padding: 5}}>
                                 <Icon
-                                    name={hidePass ? "eye-off" : "eye"}
+                                    name={hidePassword ? "eye-off" : "eye"}
                                     size={20}
-                                    color={hidePass ? "#757575" : "#FF7300"}
+                                    color={hidePassword ? "#757575" : "#FF7300"}
                                 />
                             </View>
                         </TouchableOpacity>
+                    </View>
+
+                    <View style={[styles.containerInput, {marginBottom: 15}]}>
+                        <TextInput
+                            style={styles.labelInput}
+                            placeholder="Confirme a sua senha"
+                            value={confirmPassword}
+                            onChangeText={(value) => setConfirmPassword(value)}
+                            secureTextEntry={hidePassword}
+                        />
                     </View>
 
                     <Links/>
@@ -77,12 +89,12 @@ export default function SignIn({navigation}) {
                     mode="contained"
                     uppercase={false}
                     style={styles.submit}
-                    onPress={() => Alert.alert("logou!!")}
+                    onPress={() => Alert.alert("cadastrou!!")}
                 >
-                    Login
+                    Cadastrar
                 </Submit>
 
-                <SocialButtons text="entre com"/>
+                <SocialButtons text="cadastre-se com"/>
             </View>
         </SafeAreaView>
     );
