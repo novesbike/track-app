4ef2b21e1e1f0732f7a14d816bb5322e757d73e8
Ismail Younes <isma.younes1998@gmail.com>
2021-10-26 01:40:16 -0300

    refactor: add context user authentication

---
+++ src/context/auth.context.js
@@ -0,0 +1,48 @@
+import React, { createContext, useState, useEffect } from "react";
+import { AuthService } from "services/auth.service.js";
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [isError, setIsError] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  async function checkLoggedUser() {
+    const user = AuthService.getLoggedUser();
+    const hasUserLoggedIn = !!user && user.token;
+
+    if (hasUserLoggedIn) {
+      setUser(user);
+      setIsLoggedIn(true);
+    }
+  }
+
+  useEffect(() => checkLoggedUser(), []);
+
+  const signIn = async (email, password) => {
+    return AuthService.login(email, password).then((data) => {
+      setUser(data);
+      setIsLoggedIn(true);
+    });
+  };
+
+  const logout = () => {
+    AuthService.logout();
+    setIsLoggedIn(false);
+  };
+
+  const useContext = {
+    user,
+    isLoggedIn,
+    isError,
+    setIsError,
+    signIn,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={useContext}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
