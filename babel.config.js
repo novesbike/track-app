3a419fbd4d707a24d3d228da25edf4d9daec09ad
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-25 21:27:36 -0300

    refactor: project structure

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: "./src",
+            assets: './src/assets',
+            components: './src/components',
+            constants: './src/constants',
+            data: './src/data',
+            logos: './src/logos',
+            models: './src/models',
+            navigation: './src/navigation',
+            screens: './src/screens',
+            services: './src/services',
+          },
+        },
+      ],
+    ]
   };
 };
