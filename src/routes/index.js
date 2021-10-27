29a8f8fe202472dfcef0a7223fc0e363c65158ff
Ismail Younes <isma.younes1998@gmail.com>
2021-10-26 22:06:27 -0300

    removed drawer navigation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,12 @@
 import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import AuthContext from "../context/auth.context";
 
 import AuthScreens from "./route.auth.screens";
 import BottomTabScreens from "./route.bottom-tabs";
 
 const Root = createStackNavigator();
-const Drawer = createDrawerNavigator();
-
-const DrawerStack = () => (
-  <Drawer.Navigator>
-    <Drawer.Screen name="Home" component={BottomTabScreens} />
-  </Drawer.Navigator>
-);
 
 export default () => {
   const { isLoggedIn } = useContext(AuthContext);
@@ -25,7 +17,7 @@ export default () => {
         {!isLoggedIn ? (
           <Root.Screen
             name="App"
-            component={DrawerStack}
+            component={BottomTabScreens}
             options={{ animationEnabled: false }}
           />
         ) : (
