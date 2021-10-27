381cf61561bb178b29a04148b991681e1417cae0
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-10-26 21:55:30 -0300

    add safeareaview

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Routes from "./routes/index";
 import { AuthProvider } from "./context/auth.context";
 
 const App = () => (
-  <AuthProvider>
-    <Routes />
-  </AuthProvider>
+  <SafeAreaProvider>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  </SafeAreaProvider>
 );
 
 export default App;
