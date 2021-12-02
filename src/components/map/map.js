0953b8161369680695455c2ae8a52365d48dd5eb
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-12-02 18:03:48 -0300

    add map tracking

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,43 +1,198 @@
-import * as React from "react";
-import MapView from "react-native-maps";
-import { StyleSheet, View, Dimensions } from "react-native";
+import React from "react";
+import { StyleSheet, View, Image } from "react-native";
+import MapView, {
+  Marker,
+  Polyline,
+  AnimatedRegion,
+  PROVIDER_GOOGLE,
+} from "react-native-maps";
 import * as Location from "expo-location";
+import * as TaskManager from "expo-task-manager";
+import _ from "lodash";
+import haversine from "haversine";
+import useTimer from "../../hooks/useTimer";
+import mapStyle from "styles/mapStyle.json";
+import bike from "assets/bike.png";
 
-const LATITUDE_DELTA = 0.01;
-const LONGITUDE_DELTA = 0.02;
-const LATITUDE = -25.540794;
-const LONGITUDE = -54.5832818;
+const TASK_FETCH_LOCATION = "TASK_FETCH_LOCATION";
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
 
-export default function Map() {
-  const [location, setLocation] = React.useState(null);
+const defaultCircuit = {
+  circuitOn: false,
+  prevLatLng: {},
+  coordinates: [],
+  distanceTravelled: 0,
+  averageSpeed: 0,
+  elevation: 0,
+  altitude: 0,
+  speed: 0,
+};
 
-  React.useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        alert("Permission to access location was denied");
-      }
+export default function App(props) {
+  const cronometro = useTimer();
+  const [circuit, setCircuit] = React.useState(defaultCircuit);
 
-      let location = await Location.getCurrentPositionAsync({});
+  const [userLocation] = React.useState(
+    new AnimatedRegion({
+      latitude: props.latitude,
+      longitude: props.longitude,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    })
+  );
 
-      setLocation({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      });
-    })();
+  React.useEffect(async () => {
+    await Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
+      accuracy: Location.Accuracy.BestForNavigation,
+      activityType: Location.ActivityType.Fitness,
+      showsBackgroundLocationIndicator: true,
+      deferredUpdatesInterval: 1000,
+      distanceInterval: 1,
+      timeInterval: 1000,
+      foregroundService: {
+        notificationTitle: "Noves Bike",
+        notificationBody: "O aplicativo está sendo executado em segundo plano",
+      },
+    });
+
+    return () => Location.stopLocationUpdatesAsync(TASK_FETCH_LOCATION);
   }, []);
 
-  React.useEffect(() => console.log("minha localização", location));
+  TaskManager.defineTask(
+    TASK_FETCH_LOCATION,
+    async ({ data: { locations }, error }) => {
+      if (error) return;
+
+      const {
+        timestamp,
+        coords: { latitude, longitude, altitude, speed },
+      } = locations[0];
+      const { coordinates, distanceTravelled } = circuit;
+
+      const newCoordinate = {
+        latitude,
+        longitude,
+        timestamp,
+      };
+
+      onChangeMarker(newCoordinate);
+
+      if (true) {
+        setCircuit({
+          ...circuit,
+          coordinates: coordinates.concat([newCoordinate]),
+          distanceTravelled:
+            distanceTravelled + calcDistance(newCoordinate, "meter"),
+          prevLatLng: newCoordinate,
+          averageSpeed: calcAvgSpeed(distanceTravelled),
+          altitude,
+          speed,
+        });
+      }
+    }
+  );
+
+  /**
+   * Atualiza o marcador com a localização atual do usuário
+   * @param {*} newCoordinate
+   */
+  const onChangeMarker = (newCoordinate) => {
+    userLocation.timing(newCoordinate).start();
+  };
+
+  /**
+   * Configura a região inicial no mapa de acordo com localização do usuário
+   * @returns
+   */
+  const getMapRegion = () => ({
+    latitude: props.latitude,
+    longitude: props.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  });
+
+  /**
+   * Calcula a distância entre duas coordenadas utilizando a formula de Haversine
+   * unit: km, mile, meter, nmi
+   * @param {*} newLatLng
+   * @param {*} unit
+   * @returns float
+   */
+  const calcDistance = (newLatLng, unit = "meter") => {
+    const { prevLatLng } = circuit;
+    return haversine(prevLatLng, newLatLng, { unit }) || 0;
+  };
+
+  /**
+   * Calcula a velocidade média percorrida
+   * @param {*} distanceTravelled
+   * @returns float
+   */
+  const calcAvgSpeed = (distanceTravelled) => {
+    return parseFloat(distanceTravelled / cronometro.timer) || 0;
+  };
+
+  /**
+   * Sinaliza para gravar dados no circuito
+   * @returns
+   */
+  const startCircuit = () => {
+    if (circuit.circuitOn) return;
+
+    setCircuit({
+      ...circuit,
+      circuitOn: true,
+    });
+  };
+
+  /**
+   * Sinaliza para de impedir de receber novos dados
+   * @returns
+   */
+  const stopCircuit = () => {
+    if (circuit.circuitOn) {
+      setCircuit({
+        ...circuit,
+        circuitOn: false,
+      });
+    }
+  };
+
+  /**
+   * Reinicia o circuito com dados padrão
+   * @returns
+   */
+  const resetCircuit = () => {
+    setCircuit(defaultCircuit);
+  };
+
+  const controlCircuit = {
+    start: startCircuit,
+    stop: stopCircuit,
+    clear: resetCircuit,
+  };
 
   return (
     <MapView
       style={styles.map}
-      initialRegion={location}
-      showsUserLocation
+      customMapStyle={mapStyle}
+      provider={PROVIDER_GOOGLE}
       loadingEnabled
-    />
+      initialRegion={getMapRegion()}
+    >
+      <Polyline
+        lineDashPattern={[1]}
+        lineCap={"square"}
+        coordinates={circuit.coordinates}
+        strokeWidth={6}
+        strokeColor="#f2b659"
+      />
+
+      <Marker.Animated coordinate={userLocation} anchor={{ x: 0.5, y: 0.5 }}>
+        <Image source={bike} />
+      </Marker.Animated>
+    </MapView>
   );
 }
 
--- src/screens/tracking/map.screen.js
+++ src/screens/tracking/map.screen.js
@@ -1,14 +1,59 @@
 import * as React from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, View, Dimensions, ActivityIndicator } from "react-native";
 import Map from "components/map/map";
+import * as Location from "expo-location";
 import Controls from "components/map/map.controls";
 
 const width = Math.round(Dimensions.get("window").width);
 
 export default function MapScreen(props) {
+  const [ready, setReady] = React.useState(false);
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      let { status: fore_status } =
+        await Location.requestForegroundPermissionsAsync();
+      if (fore_status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      // let { status: back_status, ...props } = await Location.requestBackgroundPermissionsAsync();
+      // console.log("props", props);
+
+      // if (back_status !== "granted") {
+      //   setErrorMsg("Permission to access location was denied");
+      //   return;
+      // }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+      setReady(true);
+    })();
+  }, []);
+
+  if (!ready) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="white" />
+      </View>
+    );
+  } else if (errorMsg) {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMsg}</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Map />
+      <Map {...location} />
       <View style={[styles.controls]}>
         <Controls {...props} />
       </View>
@@ -18,6 +63,7 @@ export default function MapScreen(props) {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     width: width,
     position: "relative",
     backgroundColor: "#fff",
